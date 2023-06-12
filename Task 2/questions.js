c865e86bfbeeee5d34a59535361310db0f44ac3e
arpita2105 <136358528+arpita2105@users.noreply.github.com>
2023-06-12 22:31:48 +0530

    Add files via upload

---
+++ Task 2/questions.js
@@ -0,0 +1,59 @@
+//an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which attribute set the text direction?",
+    answer: "dir",
+    options: [
+      "dir",
+      "lang",
+      "direction",
+      "sub"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does W3C stand for?",
+    answer: "World Wide Web consortium",
+    options: [
+      "World Wide Websites community",
+      "World Wide Web community",
+      "World Wide Websites consortium",
+      "World Wide Web consortium"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Choose the correct option related to XML. ",
+    answer: "it is a storage format but a textual storage format which can be called a language",
+    options: [
+      "it cannot be used as a database",
+      "it can be used as a database",
+      "it is a storage format but a textual storage format which can be called a language",
+      "it is not a database, it is a language"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The full form of DOM is?",
+    answer: "Document Object Model",
+    options: [
+      "Document-Oriented Memory",
+      "Document Object Model",
+      "Document Object Memory",
+      "None"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does MIME stand for?",
+    answer: "Multipurpose Internet Mail Extension",
+    options: [
+      "Multipurpose Internet Mail Extra",
+      "Multi Internet Mail End",
+      "Multipurpose Internet Mail Email",
+      "Multipurpose Internet Mail Extension"
+    ]
+  },
+  
+];
